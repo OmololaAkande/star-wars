15e7fc0e8f5688f9347a3ebed5fa493cd4e550c5
omolola <lolaakande3@gmail.com>
2023-04-22 00:25:15 +0100

    my star-war project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import WebsiteBody from './components/WebsiteBody';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <WebsiteBody/>
+    </div>
+  )
+}
+
+export default App
